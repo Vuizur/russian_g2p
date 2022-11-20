df00f239b33d8f143326fd1560ed5cdef2f17479
Vuizur <Vuizur@users.noreply.github.com>
2022-11-20 16:25:25 +0100

    Change DAWG version to fork with 3.9/3.10 support

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         'lxml',
-        'dawg',
+        'DAWG @ https://github.com/pytries/DAWG.git@actions',
         'rnnmorph @ git+https://github.com/IlyaGusev/rnnmorph.git'
     ],
 
